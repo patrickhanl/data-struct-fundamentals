c7b6c576d2ec59fa38f788885a2d1f69e4720f89
Patrick Hanley <patrickhanl@gmail.com>
2020-06-01 09:39:07 -0700

    Initial Commit

---
+++ brackets/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  brackets
+//
+//  Created by Patrick Hanley on 6/1/20.
+//  Copyright © 2020 Patrick Hanley. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
